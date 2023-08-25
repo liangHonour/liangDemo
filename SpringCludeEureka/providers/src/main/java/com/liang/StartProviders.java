0216811fe4d1615b36e716a572736000044fa234
rongyao.liang <liangglory@163.com>
2023-08-25 11:03:01 +0800

    Eureka 项目整理

--- SpringCludeEureka/providers/src/main/java/com/liang/StartProviders.java
+++ SpringCludeEureka/providers/src/main/java/com/liang/StartProviders.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.EnableMBeanExport;
-import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
 @EnableEurekaClient
